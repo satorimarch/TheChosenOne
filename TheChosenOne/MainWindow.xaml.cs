ea775fe1c5b6bdcb799ca24edb7af8225c32a98f
satorimarch <3275494617@qq.com>
2022-04-15 00:33:42 +0800

    init

--- TheChosenOne/MainWindow.xaml.cs
+++ TheChosenOne/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -13,6 +16,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Timer = System.Timers.Timer;
+
 namespace TheChosenOne
 {
     /// <summary>
@@ -20,9 +25,116 @@ namespace TheChosenOne
     /// </summary>
     public partial class MainWindow : Window
     {
+        enum Mode
+        {
+            Sequential,
+            firstrandom,
+            random,
+        }
+
+        private Mode mode;
+        private int maxNumber;
+        private int minNumber;
+        private int currNumber;
+        private bool gameStart;
+        private Random random = new Random();
+        private Timer TimerChangeNumber = new Timer();
+        private Timer TimerDrawNumber = new Timer();
+
         public MainWindow()
         {
+            gameStart = false;
+
+            TimerDrawNumber.Elapsed += (o, e) =>
+            {
+                TBNumber.Dispatcher.BeginInvoke(
+                    new Action(
+                        delegate
+                        {
+                            TBNumber.Text = currNumber.ToString();
+                        }
+                    )
+                );
+            };
+
             InitializeComponent();
+            Init_Setting();
+        }
+
+
+        internal void Init_Setting()
+        {
+            TimerChangeNumber.Dispose();
+            TimerChangeNumber = new Timer();
+
+            TimerChangeNumber.Interval = setting1.Default.Interval;
+            TimerDrawNumber.Interval = setting1.Default.DrawInterval;
+            minNumber = setting1.Default.MinNumber;
+            maxNumber = setting1.Default.MaxNumber;
+
+            TBNumber.Text = minNumber.ToString();
+
+            mode = (Mode)setting1.Default.Mode;
+
+            switch (mode) {
+                case Mode.Sequential:
+                    TimerChangeNumber.Elapsed += TickSequential;
+                    break;
+
+                case Mode.firstrandom:
+                    TimerChangeNumber.Elapsed += TickSequential;
+                    break;
+
+                case Mode.random:
+                    TimerChangeNumber.Elapsed += TickRandom;
+                    break;
+            }
+        }
+
+
+        private void TickSequential(object sender, ElapsedEventArgs e)
+        {
+            if (currNumber < maxNumber) currNumber++;
+            else currNumber = minNumber;
+            Console.WriteLine($"{currNumber} id:{Thread.CurrentThread.ManagedThreadId}");
+        }
+
+
+        private void TickRandom(object sender, ElapsedEventArgs e)
+        {
+            currNumber = random.Next(minNumber, maxNumber);
+        }
+
+
+        private void Button_Click_Pause(object sender, RoutedEventArgs e)
+        {
+            if (gameStart) {
+
+                TimerChangeNumber.Stop();
+                TimerDrawNumber.Stop();
+                ButtonPause.Content = "开始";
+                gameStart = false;
+            }
+            else {
+                ButtonPause.Content = "结束";
+                gameStart = true;
+                if (mode == Mode.firstrandom) {
+                    currNumber = random.Next(minNumber, maxNumber);
+                }
+
+                TimerChangeNumber.Start();
+                TimerDrawNumber.Start();
+            }
+        }
+
+        private void Button_Click_Open_Setting(object sender, RoutedEventArgs e)
+        {
+            if (gameStart) {
+                Button_Click_Pause(null, null);
+            }
+            SettingWindow settingWindow = new SettingWindow();
+            settingWindow.Owner = this;
+            settingWindow.ShowDialog();
         }
     }
 }
---
+++ TheChosenOne/SettingWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TheChosenOne
+{
+    /// <summary>
+    /// SettingWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class SettingWindow : Window
+    {
+        private int CheckedMode;
+        private setting1 setting = setting1.Default;
+
+        public SettingWindow()
+        {
+            InitializeComponent();
+            InitContent();
+        }
+
+        private void InitContent()
+        {
+            TextBox_Interval.Text = setting.Interval.ToString();
+            TextBox_DrawInterval.Text = setting.DrawInterval.ToString();
+            TextBox_MinNumber.Text = setting.MinNumber.ToString();
+            TextBox_MaxNumber.Text = setting.MaxNumber.ToString();
+
+            ((RadioButton)FindName("RadioBtn_mode_" + setting.Mode.ToString())).IsChecked = true;
+            //throw new NotImplementedException();
+        }
+
+        private void Button_Confirm_change(object sender, RoutedEventArgs e)
+        {
+            try {
+                setting.Interval = int.Parse(TextBox_Interval.Text);
+                setting.DrawInterval = int.Parse(TextBox_DrawInterval.Text);
+                setting.MinNumber = int.Parse(TextBox_MinNumber.Text);
+                setting.MaxNumber = int.Parse(TextBox_MaxNumber.Text);
+                setting.Mode = CheckedMode;
+
+                ((MainWindow)Owner).Init_Setting();
+            }
+            catch {
+                MessageBox.Show("输入无效", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally {
+                this.Close();
+                MessageBox.Show("修改成功", "消息", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void RadionBtn_mode_Checked(object sender, RoutedEventArgs e)
+        {
+            string name = ((RadioButton)sender).Name;
+            CheckedMode = int.Parse(name.Substring(name.LastIndexOf('_') + 1));
+        }
+    }
+}
